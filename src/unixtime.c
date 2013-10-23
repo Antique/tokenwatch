a2ec8a1dc721aa79cae8d37fe4ca41b23b5d1e4a
Peter Krempa <pkrempa@redhat.com>
2013-10-23 11:30:10 +0100

    unixtime: Rip out unneeded stuff and switch to user specific conf
    Right now, there's no reasonable IMPL to do the time config stuff in an
    automatic way. Let's rip it out and make it user configurable.
    
    Also the leap second compensation is not needed. Either pebble fixed it
    or I didn't have correct time at firts.

--- src/unixtime.c
+++ src/unixtime.c
@@ -3,32 +3,17 @@
 
 #include <time.h>
 
-static bool
-is_dst(void)
-{
-    /* TODO: */
-    /* DST SUCKS!@!@!!! */
-    /* DST in CET is from
-     * last sunday in March, 00:03:00, to
-     * last sunday in October, 00:02:00, but this check will slip 1 hour */
-
-    return true;
-    //return false;
-}
-
 uint32_t
 unixtime(void)
 {
     time_t localtime = time(NULL);
 
-    /* compensate leap seconds */
-    localtime += OFFSET_LEAP_SECONDS;
-
     /* compensate DST and timezone */
     localtime -= OFFSET_TIMEZONE;
 
-    if (is_dst())
-        localtime -= 3600;
+#ifdef OFFSET_DST
+    localtime -= 3600;
+#endif
 
    return localtime;
 }
--- src/unixtime.h
+++ src/unixtime.h
@@ -1,6 +1,4 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* pebble SDK doesn't treat unix time as it should; compensate for it */
-static const uint32_t OFFSET_LEAP_SECONDS = 8;
 uint32_t unixtime(void);
